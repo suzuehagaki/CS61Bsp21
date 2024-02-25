d5e41d8752667ea128c4a50640ec8052e40ef0d0
”suzuehagaki“ <862861415@qq.com>
2024-02-26 06:19:18 +0800

    add equalsTest's cases

--- proj1/deque/DequeTest.java
+++ proj1/deque/DequeTest.java
@@ -41,14 +41,28 @@ public class DequeTest {
     @Test
     public void equalsTest() {
         ArrayDeque<Integer> A = new ArrayDeque<>();
+        ArrayDeque<Integer> A2 = new ArrayDeque<>();
         LinkedListDeque<Integer> L = new LinkedListDeque<>();
+        LinkedListDeque<Integer> L2 = new LinkedListDeque<>();
         for (int i = 0; i < 10; i += 1) {
             A.addLast(i);
             L.addLast(i);
+            A2.addLast(i);
+            L2.addLast(i);
             assertTrue(A.equals(L));
             assertTrue(L.equals(A));
         }
         assertTrue(A.equals(A));
         assertTrue(L.equals(L));
+        assertTrue(A.equals(A2));
+        assertTrue(L.equals(L2));
+        ArrayDeque<ArrayDeque<Integer>> A3 = new ArrayDeque<>();
+        LinkedListDeque<LinkedListDeque<Integer>> L3 = new LinkedListDeque<>();
+        A3.addLast(A);
+        L3.addLast(L);
+        A3.addLast(A2);
+        L3.addLast(L2);
+        boolean b = A3.equals(L3);
+        assertTrue(b);
     }
 }
