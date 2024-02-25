2d1216bef4b51ff2aecd768666270ca931eff099
”suzuehagaki“ <862861415@qq.com>
2024-02-26 00:25:48 +0800

    delete isEmpty function since it's default

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -41,10 +41,6 @@ public class ArrayDeque<T> implements Deque<T> {
         }
     }
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
     public int size() {
         return size;
     }
