48bc3dc7486d58585306b307614e6e407181a7ee
”suzuehagaki“ <862861415@qq.com>
2024-02-26 22:54:26 +0800

    complete proj1ec

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,55 @@
+package tester;
+
+import static org.junit.Assert.*;
+
+import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+
+    @Test
+    public void randomizedTest() {
+        StudentArrayDeque<Integer> AD = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ADS = new ArrayDequeSolution<>();
+
+        StringBuilder operationSequence = new StringBuilder("\n");
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 5);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                AD.addLast(randVal);
+                ADS.addLast(randVal);
+                operationSequence.append("addLast(" + randVal + ")\n");
+            } else if (operationNumber == 1) {
+                // addFirst
+                int randVal = StdRandom.uniform(0, 100);
+                AD.addFirst(randVal);
+                ADS.addFirst(randVal);
+                operationSequence.append("addFirst(" + randVal + ")\n");
+            } else if (operationNumber == 2) {
+                // size
+                assertEquals(operationSequence.toString(), ADS.size(), AD.size());
+            } else if (operationNumber == 3) {
+                // removeLast
+                if (!AD.isEmpty()) {
+                    Integer a = AD.removeLast();
+                    Integer b = ADS.removeLast();
+                    operationSequence.append("removeLast(): " + a + "\n");
+                    assertEquals(operationSequence.toString(), b, a);
+                }
+            } else if (operationNumber == 4) {
+                // removeFirst
+                if (!AD.isEmpty()) {
+                    Integer a = AD.removeFirst();
+                    Integer b = ADS.removeFirst();
+                    operationSequence.append("removeFirst(): " + a + "\n");
+                    assertEquals(operationSequence.toString(), b, a);
+                }
+            }
+        }
+    }
+}
