d188277fa1e3113b85cbab593e909bad75daaa89
”suzuehagaki“ <862861415@qq.com>
2024-03-01 19:13:12 +0800

    fixed Main.java

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -38,8 +38,6 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        CapersRepository.setupPersistence();
-
         if (args.length == 0) {
             Utils.exitWithError("Must have at least one argument");
         }
@@ -55,9 +53,7 @@ public class Main {
                 break;
             case "dog":
                 validateNumArgs("dog", args, 4);
-                Dog d = new Dog(args[1], args[2], Integer.parseInt(args[3]));
-                System.out.println(d.toString());
-                d.saveDog();
+                CapersRepository.makeDog(args[1], args[2], Integer.parseInt(args[3]));
                 break;
             case "birthday":
                 validateNumArgs("birthday", args, 2);
